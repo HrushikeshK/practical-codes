d748bc0dd9af91ad39c94d7df3d4c66f2fab2048
tjdevworks <tejas.mahajan121@gmail.com>
2017-04-29 10:56:16 +0530

    Comments Added

--- Sliding Window/client.py
+++ Sliding Window/client.py
@@ -2,24 +2,28 @@ from __future__ import print_function
 import socket
 import random
 
+#Creating a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 host = socket.gethostname()                           
 port = 9999
+#Connecting to local host change host to other pc's ip if you want to run client and server on two different machines accordingly change in server.py
 s.connect((host, port))                               
 
 while True:
+        #Accepting protocol choice from user
         choice = int(raw_input("\n1. Enter 1 for Go Back N Protocol\n2. Enter 2 for Selective Repeat Protocol\n3. Enter 3 to exit\nEnter your choice : "))
         s.send(str(choice))
         if(choice == 3 or (choice!=1 and choice!=2)):
                 break
         
+        #Accepting window size from user
         window_size = raw_input("\nEnter the window size : ")
         s.send(window_size)
         
         message = []
         print("Receiving frames")
         
-        # Receive Message Length
+        #Receive Message Length
         len_msg = int(s.recv(10))
         s.send("Message Length is received")
         for i in range(len_msg):
@@ -29,14 +33,18 @@ while True:
         
         #Randomly drop a frame 
         random_frame_no = int(random.random() * len_msg)
+        
         if(choice == 1):
                 message = message[:random_frame_no]
         elif(choice==2):
                 message[random_frame_no] = "<NACK_SENT>"
+        
         print("Message received till now : ", "".join(message))
         print("Sending NACK randomly for frame no. : ",random_frame_no+1)
+        #NACK sent by client
         s.send(str(random_frame_no))
         
+        #Accepting lost frames depending on protocol choice
         if(choice == 1):
                 for i in range(len_msg - random_frame_no):
                         a = s.recv(1)
--- Sliding Window/server.py
+++ Sliding Window/server.py
@@ -2,20 +2,24 @@ from __future__ import print_function
 from socket import *
 import time
 
+#Creating server socket
 s = socket(AF_INET, SOCK_STREAM)
 s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
 host = gethostname()                           
 port = 9999
 
+#Binding local host and port no. to socket. Change the host ip if you want to run server the client on different machines. Currently set to loopback address
 s.bind((host, port))                                  
 
 s.listen(5)                                   
 
 print("Server has started")
+#Accept a client requesting for connection
 c,addr = s.accept()
 print("Connection established with : ",c.getsockname())
 
+#Function to send the message initially
 def sending_message(message):
         split_message = [message[i:i+window_size] for i in range(0, len(message), window_size)]
         
@@ -42,6 +46,7 @@ while True:
                 c.close()
                 break
         
+        #Receiving Window size from client
         window_size = int(c.recv(100))
         print("Window Size : ",window_size)
         
@@ -54,6 +59,7 @@ while True:
                 print("NACK has been received for sequence no : ", frame_drp_no+1)
                 split_message = [message[i:i+window_size] for i in range(frame_drp_no, len(message), window_size)]
                 
+                #Resends all frames from the point after which the frame for which NACK was received.
                 print("Resending Message : ", split_message)
                 #Resending the frames from the one after they are dropped
                 for msg in split_message:
@@ -73,6 +79,7 @@ while True:
                 print("NACK has been received for sequence no : ", frame_drp_no+1)
                 resend_frame = message[frame_drp_no]
                 
+                #Resends only that frame for which NACK was received
                 print("Resending Message : ", resend_frame)
                 c.send(resend_frame)
 
